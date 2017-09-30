7980520640c68cb36e45737df58880bce0f6c967
Jacob Dannhausen-Brun <jdannhausenbrun@gmail.com>
2017-09-30 08:02:47 -0500

    Navigation fixed

--- reflection_slideshow/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".content-window").css("margin-top", $(".navbar").height() );
-    $("body").css("pardding-top", $(".navbar").height());
-
-    // $(".navbar-toggle").on("click", function () {
-    	// $(".content-window").css("margin-top", $(".navbar").height() );
-    // });
-});
