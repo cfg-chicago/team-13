ea6becd4c400b67d4c488d67d5228f25dbe722e3
Haoran Xiao <hxiao7463@gmail.com>
2017-09-30 05:43:37 -0400

    pure frontend reflection and slideshow half done

---
+++ reflection_slideshow/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".content-window").css("margin-top", $(".navbar").height() );
+    $("body").css("pardding-top", $(".navbar").height());
+
+    // $(".navbar-toggle").on("click", function () {
+    	// $(".content-window").css("margin-top", $(".navbar").height() );
+    // });
+});
