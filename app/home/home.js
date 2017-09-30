a4cefa672ca6043c97e2c704b79c62b02ffdb32e
Ali Zarrabi <azarrabi@umich.edu>
2017-09-30 02:03:46 -0500

    merge

--- app/home/home.js
+++ app/home/home.js
@@ -8,11 +8,41 @@ angular.module('myApp.home', ['ngRoute'])
         templateUrl: 'home/home.html',
         controller: 'HomeCtrl'
     });
+    $routeProvider.when('/memories', {
+        templateUrl: 'memories/memories.html',
+        controller: 'MemoriesCtrl'
+    });
+    $routeProvider.when('/reflections', {
+        templateUrl: 'reflections/reflections.html',
+        controller: 'ReflectionsCtrl'
+    });
+    $routeProvider.when('/feedback', {
+        templateUrl: 'feedback/feedback.html',
+        controller: 'FeedbackCtrl'
+    });
 }])
  
 // Home controller
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', [function($scope) {
  
 }]);
 
+.controller('MemoriesCtrl', [function($scope) {
+ 
+}]);
+
+.controller('ReflectionsCtrl', [function($scope) {
+ 
+}]);
+
+.controller('FeedbackCtrl', [function($scope) {
+ 
+}]);
+
+$routeProvider.otherwise({
+        redirectTo: '/home'
+    });
+
 <script src="home/home.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular-route.js"></script>
+
